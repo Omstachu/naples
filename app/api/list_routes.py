b2aad0c7fd949f64d307086d4738115264ef284e
Brandon <omstachu626@gmail.com>
2021-08-26 00:47:16 -0700

    readded login required on backend routes

--- app/api/list_routes.py
+++ app/api/list_routes.py
@@ -6,14 +6,14 @@ list_routes = Blueprint('lists', __name__)
 
 
 @list_routes.route('/')
-# @login_required
+@login_required
 def lists():
     lists = List.query.join(Item)
     # lists = db.session.Query(List).join(Item)
     return {"lists": [list.to_dict() for list in lists]}
 
 @list_routes.route('/<int:id>')
-# @login_required
+@login_required
 def list(id):
     list = List.query.get(id)
     return list.to_dict()
