40ed9bfb7992515cba9bdc821c87e8175f1e6b60
Brandon <omstachu626@gmail.com>
2021-08-31 10:34:32 -0700

    adding functionality to hide forms on list page

--- react-app/src/components/EditListForm/index.js
+++ react-app/src/components/EditListForm/index.js
@@ -1,28 +1,63 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { updateListName } from "../../store/list";
+import { confirmButtonImage, cancelButtonImage } from "../images/imgSources";
 
 const EditListForm = ({list, refresher}) => {
     const [name, setName] = useState("")
+    const [showForm, setShowForm] = useState(false)
 
     const dispatch = useDispatch()
 
     useEffect(() => {
         setName(list?.name)
-    }, [list])
+    }, [list, showForm])
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         list.name = name
         await dispatch(updateListName(list))
         // hideForm()
+        setShowForm(!showForm)
         refresher()
+    }
+
+    const handleFormToggle = async (e) => {
+      e.preventDefault();
+      setShowForm(!showForm)
+
     }
 
     const updateName = (e) => {
         const name = e.target.value;
         setName(name)
     }
+
+    let formContent = null;
+
+    if (showForm){
+      formContent = (
+        <>
+        <form className="edit-item-form" onSubmit={handleSubmit}>
+
+        <input
+            placeholder={name}
+            type="text"
+            value={name}
+            onChange={updateName}
+            maxLength="40"
+        />
+        <button className="confirm-button" type="submit">
+        <img className="confirm-button-image" src={confirmButtonImage} alt="confirm-button"/>
+        </button>
+        <button className="cancel-button" onClick={handleFormToggle}>
+        <img className="cancel-button-image" src={cancelButtonImage} alt="cancel-button"/>
+        </button>
+        </form>
+        </>
+      )
+    }
+
 
     return (
         <form onSubmit={handleSubmit}>
