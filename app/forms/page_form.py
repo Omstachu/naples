fa5a2f8ae89f52c3a0fe5085a707006e1e9c26c2
Brandon <omstachu626@gmail.com>
2021-09-01 15:11:15 -0700

    added a lot of styling to signup page

--- app/forms/page_form.py
+++ app/forms/page_form.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired
 
 
 class PageForm(FlaskForm):
-    userId = IntegerField("userId")
+    userId = IntegerField("userId", validators=[DataRequired()])
     name = StringField("name")
